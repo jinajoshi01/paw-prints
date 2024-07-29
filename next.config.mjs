39e7478bb8f2b0ec765ac3f74e785da8484e6482
jinajoshi01 <jinajoshi4@gmail.com>
2024-07-29 13:24:16 +0545

    Deployed Website

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Paw-prints-app/' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/Paw-prints-app' : '',
+    trailingSlash: true,
+  };
+  
+  export default nextConfig;
+  
